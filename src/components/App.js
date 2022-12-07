2918106aa32bb49e0eda6c9a6cfa5a626fbe68da
Jacob Darker <41199879+Darker21@users.noreply.github.com>
2022-12-07 00:45:33 +0000

    Undo holding page work

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,11 @@
 import './App.css';
 import Header from './header.js';
-import {BrowserRouter as Router, Routes, Route, useLoaderData} from 'react-router-dom';
-import { lazy, Suspense } from 'react';
-import Loader from './loader.js'
 
-const HoldingPage = lazy(() => import('../pages/holding-page'));
 
 function App() {
   return (
     <>
-    <Router>
-      <Suspense fallback={Loader}>
-        <Routes>
-          <Route path='/' element={<HoldingPage />} />
-          <Route path='/*' element={<HoldingPage />} />
-        </Routes>
-      </Suspense>
-    </Router>
-    {/* <Header /> */}
+    <Header />
     </>
   );
 }
